95f17aae8d05acb049f817bebcdf30cba65d4d51
gilmore867 <ddgilmore@bellsouth.net>
2020-01-08 19:32:48 -0500

    Update ssh_server.pp

--- manifests/ssh_server.pp
+++ manifests/ssh_server.pp
@@ -10,7 +10,7 @@ class profile::ssh_server {
                   ensure => present,
                   user   => 'root',
                   type   => 'ssh-rsa',
-                  key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDK8MOQLpdpNqVTM7FxPSwVSp5Hmvf1GMB14++HTsMiIZ7ObSSirhEhFKrOA37WPbYybgR1qCow0TAPhivEiM6TzM3mGWEkHTPRrittndDswAIj7W9qKuDw8JF2Hz14dYGKHKKZlcSEYIzpB2g3Iy6g6jtBjmJjfdgPra4T4+XWs6UBVdjaHIWJdQ7GDPROh6AD9erotiUYaMVNIG3emVzopDwtMHhL73d0RXoKt9SNsANg4FgKxBuj+kHjyfWZsZVkW8mVwS2yESCpAQ5BOhMML+Nx5uIXt2fAPnrYRL3CpG89suTZFC6eK192i9DXLI+ZxL0GJMbTC/qeZN+oJ1y3',
+                  key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDK8MOQLpdpNqVTM7FxPSwVSp5Hmvf1GMB14++HTsMiIZ7ObSSirhEhFKrOA37WPbYybgR1qCow0TAPhivEiM6TzM3mGWEkHTPRrittndDswAIj7W9qKuDw8JF2Hz14dYGKHKKZlcSEYIzpB2g3Iy6g6jtBjmJjfdgPra4T4+XWs6UBVdjaHIWJdQ7GDPROh6AD9erotiUYaMVNIG3emVzopDwtMHhL73d0RXoKt9SNsANg4FgKxBuj+kHjyfWZsZVkW8mVwS2yESCpAQ5BOhMML+Nx5uIXt2fAPnrYRL3CpG89suTZFC6eK192i9DXLI+ZxL0GJMbTC/qeZN+oJ1y3',
          }
   }
         
